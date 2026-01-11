9c4d59c716142f10dd59d8dab5d3aa5a90c3870f
Divy <divya.zoom12@gmail.com>
2026-01-11 15:58:14 +0530

    working on todo

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let allElems = document.querySelectorAll('.elem');
-let fullElemPage = document.querySelectorAll('.fullElem')
-let fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
+function openFeatures() {
+  let allElems = document.querySelectorAll(".elem");
+  let fullElemPage = document.querySelectorAll(".fullElem");
+  let fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function(elem){
-    elem.addEventListener('click', function(){
-        fullElemPage[elem.id].style.display = 'block'
-    })
-})
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
+    });
+  });
 
-fullElemPageBackBtn.forEach(function(back){
-    back.addEventListener('click', function(){
-        fullElemPage[back.id].style.display = 'none'
-    })
-})
+  fullElemPageBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
+  });
+}
+
+openFeatures()
+
