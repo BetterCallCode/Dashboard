e2fd646d201edfaa93581c371a7d6b62c55ee346
Divy <divya.zoom12@gmail.com>
2026-01-12 23:35:24 +0530

    mark as imp added

--- script.js
+++ script.js
@@ -16,12 +16,50 @@ function openFeatures() {
   });
 }
 
-openFeatures()
+openFeatures();
 
-let form =  document.querySelector('.addTask form')
-let taskInput = document.querySelector('.addTask form input')
-let taskDetailsInput = document.querySelector('.addTask form textarea')
+let form = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form input");
+let taskDetailsInput = document.querySelector(".addTask form textarea");
+let taskCheckbox = document.querySelector(".addTask form #check");
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-})
+let currentTask = [
+  {
+    task: "Hello world",
+    details: "Lala ram babu shayam",
+    imp: true,
+  },
+  {
+    task: "Coding",
+    details: "react js",
+    imp: true,
+  },
+  {
+    task: "Maths",
+    details: "basic maths",
+    imp: false,
+  },
+];
+
+function renderTask() {
+  let allTask = document.querySelector(".allTask");
+
+  let sum = "";
+
+  currentTask.forEach(function (elem) {
+    sum += `<div class="task">
+              <h5>${elem.task} <span class=${elem.imp}>Imp</span></h5>
+              <button>Mark as Completed</button>
+          </div>
+                `;
+  });
+
+  allTask.innerHTML = sum;
+}
+
+renderTask();
+
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
